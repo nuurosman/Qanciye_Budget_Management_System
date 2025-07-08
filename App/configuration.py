90d21d9511402217cfbdb6bc638f0f439f366279
nuurosmaan <nuurosmaannuur579@gmail.com>
2025-07-08 17:56:10 +0300

    requirements file created

--- App/configuration.py
+++ App/configuration.py
@@ -1,22 +1,22 @@
 class UserDbConnection:
-    DB_HOSTNAME = 'localhost'
-    DB_USERNAME = 'root'
+    DB_HOSTNAME = '185.7.81.97'
+    DB_USERNAME = 'fahad '
     DB_NAME = 'qanciye_budget_management_system'
-    DB_PASSWORD = 'Eng_nor@2002'
+    DB_PASSWORD = 'Assaamarraaii!@12'
 
     
 class AdminDbConnetion:
-    DB_HOSTNAME = 'localhost'
-    DB_USERNAME = 'root'
+    DB_HOSTNAME = '185.7.81.97'
+    DB_USERNAME = 'fahad '
     DB_NAME = 'qanciye_budget_management_system'
-    DB_PASSWORD = 'Eng_nor@2002'
+    DB_PASSWORD = 'Assaamarraaii!@12'
 
 
 class LabourDbConnetion:
-    DB_HOSTNAME = 'localhost'
-    DB_USERNAME = 'root'
+    DB_HOSTNAME = '185.7.81.97'
+    DB_USERNAME = 'fahad '
     DB_NAME = 'qanciye_budget_management_system'
-    DB_PASSWORD = 'Eng_nor@2002'
+    DB_PASSWORD = 'Assaamarraaii!@12'
 
 
 
